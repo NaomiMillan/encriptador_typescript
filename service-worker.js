b53cac2fa625697f5cf41c4baee9af45780005b8
Naomi Millan <naomimillansanchez@gmail.com>
2025-06-07 15:12:37 -0600

    Agrego manifest, service worker, iconos y mejoras para PWA

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'cesar-app-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  // agrega aquí otros archivos estáticos que usas, como tu CSS, JS
+];
+
+// Instalación del SW y cacheo
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Interceptar peticiones y responder desde cache si está
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
